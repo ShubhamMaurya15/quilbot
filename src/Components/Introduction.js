9b5a482c466f963ecd246b0573a83fd359e22ee2
Shubham Maurya <110913843+ShubhamMaurya15@users.noreply.github.com>
2023-01-06 22:05:20 +0530

    Add files via upload

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -1,1 +1,27 @@
+import classes from "./../Button/Button.module.css";
+import { Typography, Button } from "@mui/material";
 
+const Introduction = (props) => {
+  return (
+    <div>
+      <Typography sx={{ m: 10 }} variant="h4">
+        Save time and write with confidence
+      </Typography>
+      <Button
+        className={classes.button}
+        variant="contained"
+        style={{
+          backgroundColor: `green`,
+          borderRadius: "25px",
+          fontWeight: "bolder",
+          fontSize: "larger",
+          cursor: "pointer",
+        }}
+      >
+        Upgrade to QuillBot Premium
+      </Button>
+    </div>
+  );
+};
+
+export default Introduction;
